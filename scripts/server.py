dfd491063a1cbc142df1f1f18b247fe5643ba446
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-01-09 23:02:26 +0100

    server.py

---
+++ scripts/server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""Use instead of `python3 -m http.server` when you need CORS"""
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        return super(CORSRequestHandler, self).end_headers()
+
+
+httpd = HTTPServer(('localhost', 8003), CORSRequestHandler)
+httpd.serve_forever()
