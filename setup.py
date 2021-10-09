d6e898f781a9f6d31df419ebc4cdad13c1158784
Jason Grout <jgrout6@bloomberg.net>
2021-10-08 20:07:42 -0700

    Change the name to jupyterlab-pluginplayground

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-jupyterlab-dynext setup
+jupyterlab-pluginplayground setup
 """
 import json
 import sys
@@ -10,7 +10,7 @@ import setuptools
 HERE = Path(__file__).parent.resolve()
 
 # The name of the project
-name = "jupyterlab_dynext"
+name = "jupyterlab_pluginplayground"
 
 lab_path = (HERE / name.replace("-", "_") / "labextension")
 
@@ -20,7 +20,7 @@ ensured_targets = [
     str(lab_path / "static/style.js")
 ]
 
-labext_name = "jupyterlab-dynext"
+labext_name = "jupyterlab-pluginplayground"
 
 data_files_spec = [
     ("share/jupyter/labextensions/%s" % labext_name, str(lab_path.relative_to(HERE)), "**"),
