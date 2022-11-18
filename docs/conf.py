f970d155ffd90ddcd6f33a9d33dbf4cf79e1ad8e
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-11-18 16:08:28 +0000

    Run black on conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,36 +1,31 @@
-# -*- coding: utf-8 -*-
-extensions = [
-    'myst_parser',
-    'jupyterlite_sphinx'
-]
+extensions = ["myst_parser", "jupyterlite_sphinx"]
 
 jupyterlite_config = "jupyter_lite_config.json"
 jupyterlite_dir = "."
 jupyterlite_contents = "content"
 
-master_doc = 'index'
-source_suffix = '.md'
+master_doc = "index"
+source_suffix = ".md"
 
 # General information about the project.
-project = 'JupyterLab Plugin Playground'
-author = 'Project Jupyter'
+project = "JupyterLab Plugin Playground"
+author = "Project Jupyter"
 
 exclude_patterns = []
-highlight_language = 'python'
-pygments_style = 'sphinx'
+highlight_language = "python"
+pygments_style = "sphinx"
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-html_css_files = [
-    'custom.css'
-]
+html_css_files = ["custom.css"]
 
 
 def on_config_inited(*args):
     import sys
     import subprocess
     from pathlib import Path
+
     HERE = Path(__file__)
     ROOT = HERE.parent.parent
     subprocess.check_call(["jlpm"], cwd=str(ROOT))
