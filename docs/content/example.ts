d6f21462fb93f0ed4c9d732ad79a3562a9f58a04
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-11-23 10:29:51 +0100

    Update example content

--- docs/content/example.ts
+++ docs/content/example.ts
@@ -1,13 +1,18 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin,
+} from '@jupyterlab/application';
+
 /**
  * This is an example hello world plugin.
  * Open Command Palette with Ctrl+Shift+C
  * (Command+Shift+C on Mac) and select
  * "Load Current File as Extension"
  */
-const plugin = {
+const plugin: JupyterFrontEndPlugin<void> = {
   id: 'hello-world:plugin',
   autoStart: true,
-  activate: (app) => {
+  activate: (app: JupyterFrontEnd) => {
     alert('Hello World!');
   },
 };
